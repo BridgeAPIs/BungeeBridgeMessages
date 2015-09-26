8d35b12b59829553ae1a706f9b8329d96b95f9e3
zyuiop <zyuiop@zyuiop.net>
2015-09-26 16:58:27 +0200

    First commit, imported module from BungeeBridge

---
+++ src/main/java/net/zyuiop/bungeebridgemessages/BungeeBridgeMessagesPlugin.java
@@ -0,0 +1,78 @@
+package net.zyuiop.bungeebridgemessages;
+
+import net.bridgesapis.bungeebridge.BungeeBridge;
+import net.bridgesapis.bungeebridge.commands.CommandHelp;
+import net.bridgesapis.bungeebridge.core.TasksExecutor;
+import net.bridgesapis.bungeebridge.core.database.DatabaseConnector;
+import net.bridgesapis.bungeebridge.core.database.Publisher;
+import net.bridgesapis.bungeebridge.core.database.ServerSettings;
+import net.bridgesapis.bungeebridge.core.players.PlayerDataManager;
+import net.bridgesapis.bungeebridge.core.players.UUIDTranslator;
+import net.bridgesapis.bungeebridge.core.proxies.NetworkBridge;
+import net.bridgesapis.bungeebridge.core.servers.ServersManager;
+import net.bridgesapis.bungeebridge.permissions.PermissionsBridge;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.zyuiop.bungeebridgemessages.commands.CommandMsg;
+import net.zyuiop.bungeebridgemessages.commands.CommandReply;
+
+/**
+ * @author zyuiop
+ */
+public class BungeeBridgeMessagesPlugin extends Plugin {
+    private final PrivateMessagesManager manager = new PrivateMessagesManager(this);
+    private final BungeeBridge bridge = BungeeBridge.getInstance();
+
+    public void onEnable() {
+        bridge.getConnector().subscribe("privatemessages", new PrivateMessagesHandler(manager));
+
+        getProxy().getPluginManager().registerCommand(this, new CommandMsg(this));
+        getProxy().getPluginManager().registerCommand(this, new CommandReply(this));
+
+        CommandHelp.addHelp("msg <player> <message>", "Sends a private message to a player");
+        CommandHelp.addHelp("r <message>", "Sends a private message to the last recipient");
+    }
+
+    public String getProxyName() {
+        return bridge.getProxyName();
+    }
+
+    public ServerSettings getServerSettings() {
+        return bridge.getServerSettings();
+    }
+
+    public Publisher getPublisher() {
+        return bridge.getPublisher();
+    }
+
+    public PermissionsBridge getPermissionsBridge() {
+        return bridge.getPermissionsBridge();
+    }
+
+    public NetworkBridge getNetworkBridge() {
+        return bridge.getNetworkBridge();
+    }
+
+    public ServersManager getServersManager() {
+        return bridge.getServersManager();
+    }
+
+    public UUIDTranslator getUuidTranslator() {
+        return bridge.getUuidTranslator();
+    }
+
+    public TasksExecutor getExecutor() {
+        return bridge.getExecutor();
+    }
+
+    public DatabaseConnector getConnector() {
+        return bridge.getConnector();
+    }
+
+    public PlayerDataManager getPlayerDataManager() {
+        return bridge.getPlayerDataManager();
+    }
+
+    public PrivateMessagesManager getPrivateMessagesManager() {
+        return manager;
+    }
+}
---
+++ src/main/java/net/zyuiop/bungeebridgemessages/commands/CommandReply.java
@@ -0,0 +1,44 @@
+package net.zyuiop.bungeebridgemessages.commands;
+
+
+import net.bridgesapis.bungeebridge.commands.DefaultExecutor;
+import net.bridgesapis.bungeebridge.i18n.I18n;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.zyuiop.bungeebridgemessages.BungeeBridgeMessagesPlugin;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+
+public class CommandReply extends DefaultExecutor {
+
+	private BungeeBridgeMessagesPlugin plugin;
+
+	public CommandReply(BungeeBridgeMessagesPlugin plugin) {
+		super("r", null, "reply", "rep");
+		this.plugin = plugin;
+	}
+
+
+	@Override
+	public void execute(final CommandSender arg0, final String[] arg1) {
+		if (arg1.length < 1) {
+			TextComponent msg = new TextComponent(I18n.getModuleTranslation("messages", "commands.reply.missing_argument"));
+			msg.setColor(ChatColor.RED);
+			arg0.sendMessage(msg);
+			return;
+		}
+
+		if (!(arg0 instanceof ProxiedPlayer)) {
+			TextComponent msg = new TextComponent(I18n.getTranslation("commands.misc.not_a_player"));
+			msg.setColor(ChatColor.RED);
+			arg0.sendMessage(msg);
+			return;
+		}
+
+		String message = StringUtils.join(Arrays.copyOfRange(arg1, 0, arg1.length), " ");
+		plugin.getPrivateMessagesManager().reply((ProxiedPlayer) arg0, message);
+	}
+}
